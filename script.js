fa3580fabe92d1be736818e4c3b5727644b0494b
Amra200 <amrnjr4@gmail.com>
2024-04-19 19:10:14 +0200

    newProject

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
-function displayCurrentDate() {
-  const today = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  document.getElementById("currentDate").textContent = today.toLocaleDateString(
-    "ar-EG",
-    options
-  );
+let cities = ["القاهره ", " مكه المكرمه ", "الرياض  "];
+for (city of cities) {
+  const content = `
+    <option>${city}</option>
+    `;
+  document.getElementById("cityes-select").innerHTML += content;
 }
-
-async function getPrayerTimes() {
-  const selectedCity = document.getElementById("city").value;
-  const apiUrl = `http://api.aladhan.com/v1/timingsByCity?city=${selectedCity}&country=EG&method=8`;
-
-  try {
-    const response = await axios.get(apiUrl);
+let params = {
+  country: "EG",
+  city: "cairo",
+};
+axios
+  .get("http://api.aladhan.com/v1/timingsByCity", {
+    params: params,
+  })
+  .then(function (response) {
     const timings = response.data.data.timings;
+    fillTimeForPrayer("fajr-time", timings.Fajr);
+    fillTimeForPrayer("sunrise-time", timings.Sunrise);
+    fillTimeForPrayer("dhuhr-time", timings.Dhuhr);
+    fillTimeForPrayer("asr-time", timings.Asr);
+    fillTimeForPrayer("sunset-time", timings.Sunset);
+    fillTimeForPrayer("isha-time", timings.Isha);
 
-    const prayerTimesHTML = `
-                     <h2>مواقيت الصلاة في ${selectedCity}</h2>
-                     <p>الفجر: ${timings.Fajr}</p>
-                     <p>الشروق: ${timings.Sunrise}</p>
-                     <p>الظهر: ${timings.Dhuhr}</p>
-                     <p>العصر: ${timings.Asr}</p>
-                     <p>المغرب: ${timings.Maghrib}</p>
-                     <p>العشاء: ${timings.Isha}</p>
-               `;
-    document.getElementById("prayerTimes").innerHTML = prayerTimesHTML;
-    displayCurrentDate();
-  } catch (error) {
-    console.log("Error fetching prayer times:", error);
-  }
+    const readableDate = response.data.data.date.readable;
+    const weekday = response.data.data.date.hijri.weekday.ar;
+    const date = readableDate + " " + weekday;
+    document.getElementById("date").innerHTML = date;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function fillTimeForPrayer(id, time) {
+  document.getElementById(id).innerHTML = time;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  getPrayerTimes();
-});
-
-// const images = [
-//   "./imags/35.jpg",
-//   "./imags/36.jpg",
-//   "./imags/38.jpg",
-//   "./imags/background.jpg",
-// ];
-// let currentIndex = 0;
-// const backgroundImageDiv = document.getElementById("backgroundImage");
-
-// function changeBackgroundImage() {
-//   const imageUrl = images[currentIndex];
-//   backgroundImageDiv.style.backgroundImage = `url(${imageUrl})`;
-//   currentIndex = (currentIndex + 1) % images.length;
-// }
-
-// setInterval(changeBackgroundImage, 5000);
